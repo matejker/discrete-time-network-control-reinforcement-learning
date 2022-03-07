e6d51ca9038322793b753e54c9e5f90bc7dcebaa
Matej Kerekrety <matejkerekrety@datavant.com>
2022-03-07 10:56:23 +0000

    :sparkles: configuration models

---
+++ network_control_rl_framework/network/configuration_models.py
@@ -0,0 +1,9 @@
+from .network import Network, WeightNetwork
+
+
+def erdos_renyi_random_network(nodes: int, p: float):
+    pass
+
+
+def barabasi_albert_preferential_attachment_network(nodes: int, m: int):
+    pass
