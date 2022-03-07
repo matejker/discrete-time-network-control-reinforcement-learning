6a1aa6b134cc3cd9d199485dad649388f58b7b64
Matej Kerekrety <matejkerekrety@datavant.com>
2022-03-07 15:04:04 +0000

    :sparkles: configuration models

--- network_control_rl_framework/network/configuration_models.py
+++ network_control_rl_framework/network/configuration_models.py
@@ -1,9 +1,61 @@
+import numpy as np
+from typing import Optional
+from itertools import combinations, product
+
 from .network import Network
 
 
-def erdos_renyi_random_network(nodes: int, p: float) -> Network:
-    pass
+def erdos_renyi_random_network(n: int, p: float = 0.1, seed: Optional[int] = None) -> Network:
+    if seed:
+        np.random.seed(seed)
+    edges = [e for e in product(ranger(n), repeat=2) if np.random.random() < p]
+
+    return Network(edges=edges, n=n)
 
 
-def barabasi_albert_preferential_attachment_network(nodes: int, m: int) -> Network:
-    pass
+def barabasi_albert_preferential_attachment_network(
+    n: int, m: int, m0: Optional[int] = None, seed: Optional[int] = None
+) -> Network:
+    """Barabasi-Albert model [2]
+    Args:
+        - n (integer): number of network nodes
+        - m (integer): number of new edges from newly added node
+        - m0=None (integer): size of initial connected network
+        - seed=None (integer): numpy.random.seed (integer between 0 and 2**32 - 1 inclusive) [1]
+    Returns:
+        A Network object.
+    Raises:
+        ValueError: If inserted m <= m0
+        ValueError: If inserted n => m0
+    References:
+        [1] The SciPy community, Numpy.random.seed,
+        https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.seed.html
+        [2] Newman, M. E. J. (2010), Networks: an introduction,
+        Oxford University Press, Oxford; New York
+    """
+    if seed:
+        np.random.seed(seed)
+
+    m0 = m0 or m
+
+    if m0 < m:
+        raise ValueError(f"Inserted values are not correct, m <= m0, m={m} and m0={m0}")
+
+    if n < m0:
+        raise ValueError(f"Inserted values are not correct, n => m0, m0={m0} and n={n}")
+
+    # Initial complete graph
+    edges = list(combinations(list(range(m0)), 2))
+    nodes_basket = list(range(m0)) * (m0 - 1)
+
+    for r in range(m0, n):
+        connections = random_choice(nodes_basket, m)
+        rm = [r] * m
+        for c in connections:
+            if np.random.random() < 0.5:
+                edges.append((c, r))
+            else:
+                edges.append((r, c))
+        nodes_basket.extend(rm + connections)
+
+    return Network(edges=edges, n=n)
--- network_control_rl_framework/utils.py
+++ network_control_rl_framework/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import Any
 
 
@@ -15,3 +16,21 @@ def is_float(number: Any) -> bool:
         return True
     except (ValueError, TypeError):
         return False
+
+
+def random_choice(seq, size: int = 2):
+    """Generate a random choice of given size with no repeating.
+    Args:
+        - seq (list): a list of elements to choose from, elements can be repeated
+        - size=2 (integer): size of the random set
+    Returns:
+        Set of random elements of given size
+    """
+    targets = set()
+
+    while len(targets) < size:
+        x = np.random.choice(seq)
+
+        targets.add(x)
+
+    return list(targets)
