6ac6edcfdc3701d1aaaad41976098a7fb2f3b2a2
Matej Kerekrety <matejkerekrety@datavant.com>
2022-03-09 21:54:43 +0000

    ignore typing

--- network_control_rl_framework/algebra/__init__.py
+++ network_control_rl_framework/algebra/__init__.py
@@ -12,9 +12,9 @@ class FiniteField:
     def __init__(self, a: int, p: int) -> None:
         if p in PRIMES:
             self.__class__ = _FiniteField  # type: ignore
-            self.__init__(a, p)
+            self.__init__(a, p)  # type: ignore
         elif p in POWERS_2:
             self.__class__ = BinaryFiniteField  # type: ignore
-            self.__init__(a, p)
+            self.__init__(a, p)  # type: ignore
         else:
             raise ValueError(f"{p} is neither of prime {PRIMES} or prime factor of 2 {POWERS_2}")
