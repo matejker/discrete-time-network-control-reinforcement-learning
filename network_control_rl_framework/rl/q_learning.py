83c0e48d42f7f0bf368130c0ee9286fd2bbe50e4
Matej Kerekrety <matejkerekrety@datavant.com>
2022-03-14 22:00:37 +0000

    :boom: Q-learning

--- network_control_rl_framework/rl/q_learning.py
+++ network_control_rl_framework/rl/q_learning.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import Any, Optional, Tuple, Dict
 
-from network_control_rl_framework.network import Network
+from network_control_rl_framework.network import Network, calculate_next_state_base_number
 from network_control_rl_framework.rl.model import RLModel
 from network_control_rl_framework.rl.policy import random_action
 from network_control_rl_framework.algebra import BaseNumber
@@ -39,29 +39,47 @@ class QLearning(RLModel):
         self.gamma = gamma
         self.alpha = alpha
         self.q_dict: Dict[Any, Any] = {-1: {}}  # TODO: fix typing
+        self.all_possible_action = np.arange(self.q ** self.m)  # TODO: can we do better?
 
-    def get_best_action_for_state(self, state: BaseNumber, seed: Optional[int] = None) -> Tuple[int, float]:
-        if seed:
-            np.random.seed(seed)
-
+    def get_best_action_for_state(self, state: BaseNumber) -> Tuple[BaseNumber, BaseNumber, float]:
         max_value: float = -1.0
         best_action: int = -1
+        next_state = state
 
-        already_explored_actions = self.q_dict.get(state, {})
-        for action, value in already_explored_actions.items():
+        np.random.shuffle(self.all_possible_action)
+        for action in all_possible_action:
+            temp_state = calculate_next_state_base_number(self.network, state, action, self.input_matrix)
+            value = self.q_dict.get(temp_state, {}).get(action, 0.1)
+
             if value > max_value:
                 max_value = value
                 best_action = action
+                next_state = temp_state
 
-        return best_action, max_value
+        return BaseNumber(self.m, self.q, best_action), next_state, max_value
 
     def train(self, seed: Optional[int] = None):
-        random_explore_vec = np.random.rand(size=self.num_episodes) < self.epsilon
-        for t in range(self.num_episodes):
-            # Explore
-            if random_explore_vec[t]:
-                next_action: BaseNumber = random_action(self.m, self.q)
+        if seed:
+            np.random.seed(seed)
 
-            # Exploit
-            else:
-                pass
+        for _ in range(self.num_episodes):
+            state = self.initial_state
+            action = 0
+
+            for t in range(self.max_iteration):
+                value = self.get(state, {}).get(action, 0.1)
+                next_action, next_state, max_value = self.get_best_action_for_state(state)
+
+                # Explore
+                if np.random.rand() < self.epsilon:
+                    next_action: BaseNumber = random_action(self.m, self.q)
+
+                reward = (next_state.a == self.end_state.a) * 1  # TODO: add BaseNumber equality __equal__
+                self.q_dict[state][action] = min(value + self.alpha * (reward + self.gamma * max_value - value), 1)
+
+                state = next_state
+                action = next_action
+
+                if reward:
+                    break
+    
